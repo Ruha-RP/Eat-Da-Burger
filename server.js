709899119e3a4fb4521f525e832d968c3ee0dd94
Ruha Peera <ruha.rizwan@gmail.com>
2018-01-23 08:55:12 -0500

    adds jawsdb

--- server.js
+++ server.js
@@ -11,12 +11,18 @@ var mysql = require("mysql");
 //MySQL CONNECTION
 //================
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "root",
 	database: "burgers_db"
 });
+};
 
 connection.connect(function(err) {
 	if(err) {
